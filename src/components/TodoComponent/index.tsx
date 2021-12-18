4e99b05845b3f464a6a2522868da3ced14d7dc98
Raellopes368 <raellopes368@gmail.com>
2021-12-18 14:00:30 -0300

    Opção de remoção de todo

--- src/components/TodoComponent/index.tsx
+++ src/components/TodoComponent/index.tsx
@@ -1,22 +1,41 @@
 import { useState } from "react";
-import './styles.scss';
+import { BsTrashFill } from "react-icons/bs";
+import "./styles.scss";
 
 export type TTodoComponent = {
   name: string;
-}
+  index: number;
+};
 
 type TodoComponentProps = {
   todo: TTodoComponent;
+  deleteTodo: (index: number) => void;
 };
 
-export function TodoComponent({ todo }: TodoComponentProps) {
+export function TodoComponent({ todo, deleteTodo }: TodoComponentProps) {
   const [closed, setClosed] = useState(false);
   return (
-    <li className={`todo-item ${closed && 'closed'}`}>
-      <span className="name">{todo.name}</span>
+    <li className={`todo-item ${closed && "closed"}`}>
+      <div className="todoRow">
+        <span className="name">{todo.name}</span>
+        <BsTrashFill
+          color="#e42b2b"
+          size={23}
+          onClick={() => {
+            deleteTodo(todo.index)
+          }}
+        />
+      </div>
       <div className="row">
-        <input type="checkbox" checked={closed} id={todo.name} onChange={() => setClosed(!closed)}/>
-        <label htmlFor={todo.name} className="close">{closed ? 'Abrir': 'Fechar'}</label>
+        <input
+          type="checkbox"
+          checked={closed}
+          id={todo.name}
+          onChange={() => setClosed(!closed)}
+        />
+        <label htmlFor={todo.name} className="close">
+          {closed ? "Abrir" : "Fechar"}
+        </label>
       </div>
     </li>
   );
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,19 +2,34 @@ import { useState } from "react";
 import { TTodoComponent, TodoComponent } from "../TodoComponent";
 import { InputTodo } from "../InputTodo";
 import styles from "./styles.module.scss";
+type TTodo = {
+  name: string;
+};
 
 export function TodoList() {
-  const [todos, setTodos] = useState<TTodoComponent[]>([]);
+  const [todos, setTodos] = useState<TTodo[]>([]);
   function handleSaveTodo(todo: string) {
     setTodos([{ name: todo }, ...todos]);
+  }
+
+  function handleDeleteTodo(index: number) {
+    const removedTodos = todos.filter((todo, indx) => indx !== index);
+    setTodos(removedTodos);
   }
 
   return (
     <div className={styles.container}>
       <InputTodo saveTodo={handleSaveTodo} />
       <ul className={styles.todoList}>
-        {todos.map(todo => (
-          <TodoComponent key={todo.name} todo={todo} />
+        {todos.map((todo, index) => (
+          <TodoComponent
+            key={todo.name}
+            todo={{
+              ...todo,
+              index,
+            }}
+            deleteTodo={handleDeleteTodo}
+          />
         ))}
       </ul>
     </div>
